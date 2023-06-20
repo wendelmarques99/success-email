17c4140e1ecfbdb3990a9a175606bb7df2c44cd6
wendelmarques99 <wendelmarques99@outlook.com>
2023-06-19 23:18:03 -0300

    first commit

---
+++ loginemail/app/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.mail import EmailMessage 
+from django.conf import settings
+from django.template.loader import render_to_string
+
+# Create your views here.
+def login(request):
+    if request.method == "GET":
+        return render(request, "login.html")
+    else:
+        email = request.POST.get("email")
+        senha = request.POST.get("senha")
+        data = {"user": email}
+        template = render_to_string("email.html", data)
+
+        email = EmailMessage(
+        "Login - portal", 
+        template, 
+        settings.EMAIL_HOST_USER,
+        ['wendelmarques99@outlook.com']
+        )
+        email.send()
+        return render(request, "habilitado.html", data)
